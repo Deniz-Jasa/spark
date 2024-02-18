98a7f5dd6673ad3ffd47ca950f97dff4e8fd79bd
Deniz Jasarbasic <denizj2828@gmail.com>
2024-02-17 23:28:44 -0800

    clean ui tags

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,33 @@ const containerStyle = {
 };
 
 const center = {
-	lat: 37.7749, // Latitude of San Francisco
-	lng: -122.4194 // Longitude of San Francisco
+	lat: 37.630720, 
+	lng: -122.269546 // Longitude of San Francisco
+};
+
+const getTypeStyles = (type) => {
+	switch (type) {
+		case 'in-kind':
+			return {
+				backgroundColor: '#FFF0F0', // Dark red
+				color: '#A63737'
+			};
+		case 'volunteers':
+			return {
+				backgroundColor: '#F0F8FF', // Light blue
+				color: '#1664B1' // Dark blue
+			};
+		case 'donation':
+			return {
+				backgroundColor: '#EFFFEA', // Light green
+				color: '#176B39' // Dark green
+			};
+		default:
+			return {
+				backgroundColor: 'transparent',
+				color: 'black'
+			};
+	}
 };
 
 const HomePage = () => {
@@ -29,31 +54,33 @@ const HomePage = () => {
 		setSelectedItem(null);
 	};
 
-	const onSearch = (value) => {
+	const handleSearch = (value) => {
 		const lowerCaseValue = value.toLowerCase();
 		const filtered = mockData.filter(item =>
 			item.title.toLowerCase().includes(lowerCaseValue) ||
-			item.description.toLowerCase().includes(lowerCaseValue)
+			item.description.toLowerCase().includes(lowerCaseValue) || 
+			item.type.toLowerCase().includes(lowerCaseValue) ||
+			item.location.toLowerCase().includes(lowerCaseValue)
 		);
 		setFilteredData(filtered);
-	};
+	};	
 
 	const [map, setMap] = React.useState(null);
 
 	const onLoad = React.useCallback(function callback(map) {
-	  // This is just an example of getting and using the map instance!!! don't just blindly copy!
-	  setMap(map);
+		// This is just an example of getting and using the map instance!!! don't just blindly copy!
+		setMap(map);
 	}, []);
-  
+
 	const onUnmount = React.useCallback(function callback(map) {
-	  setMap(null);
+		setMap(null);
 	}, []);
 
 	return (
 		<div style={{ padding: "20px", height: "calc(100vh - 160px)", overflowY: "auto" }}>
 			<h1 style={{ color: "#1E1E1E", textAlign: 'left', marginBottom: '20px' }}>Discover</h1>
 			<p style={{ color: "#929292", textAlign: 'left', marginBottom: '40px' }}>Discover sustainable ways to support local business and non-profit organizations (NPOs) through eco-friendly practices like in-kind donations, financial contributions, and volunteer efforts, fostering positive change in your community.</p>
-			<Search placeholder="Search..." onChange={onSearch} style={{ width: '100%', marginBottom: '20px' }} />
+			<Search placeholder="Search..." onChange={(e) => handleSearch(e.target.value)} style={{ width: '100%', marginBottom: '20px' }} />
 			<LoadScript
 				googleMapsApiKey="AIzaSyCCu4rPIMPW37A12h15LdaEUic9o79SVAc"
 				style={{ borderRadius: "10px" }}
@@ -80,8 +107,10 @@ const HomePage = () => {
 										<h3 style={{ color: 'black' }}>{item.title}</h3>
 										<br></br>
 										<p style={{ color: 'black' }}>{item.description}</p>
-										<br></br>
-										<p style={{ color: 'black' }}>{item.type}</p>
+										<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '30px' }}>
+											<p style={{ ...getTypeStyles(item.type), paddingLeft: '8px', paddingRight: '8px', borderRadius: '4px', marginRight: '8px' }}>{item.type}</p>
+											<button style={{ backgroundColor: '#ff8947', border: 'none', padding: '8px 16px', borderRadius: '4px' }}>Learn More</button>
+										</div>
 									</div>
 								</InfoWindow>
 							)}
@@ -101,8 +130,8 @@ const HomePage = () => {
 								<Meta title={item.title} description={item.description} />
 
 								<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '40px' }}>
-									<p style={{ color: '#176B39', backgroundColor: item.type === 'monetary' ? '#EFFFEA' : 'transparent', paddingLeft: '8px', paddingRight: '8px', borderRadius: '4px', marginRight: '8px' }}>{item.type}</p>
-									<button style={{ backgroundColor: 'blue', color: 'white', border: 'none', padding: '8px 16px', borderRadius: '4px' }}>Learn More</button>
+									<p style={{ ...getTypeStyles(item.type), paddingLeft: '8px', paddingRight: '8px', borderRadius: '4px', marginRight: '8px' }}>{item.type}</p>
+									<button style={{ backgroundColor: '#ff8947', border: 'none', padding: '8px 16px', borderRadius: '4px' }}>Learn More</button>
 								</div>
 
 							</Card>
